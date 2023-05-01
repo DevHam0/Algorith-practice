334c0be062226e81d83a042cf3a2738b58cd3841
backwizard0 <gkaeogus7@gmail.com>
2023-05-02 01:52:30 +0900

    mm

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+
+
+class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/Part1/Q2744.java
@@ -0,0 +1,22 @@
+package Part1;
+
+import java.util.Scanner;
+
+public class Q2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //ASCII CODE
+//        char x = 65;
+//        System.out.println('Z' - 'A');      // 25
+//        System.out.println(x);              // A
+//        System.out.println((int)x);         // 65
+//        System.out.println((char)65);       // A
+//        System.out.println(x + 25);         // 90
+//        System.out.println((char)(x + 25)); // Z
+
+        String input = sc.nextLine();
+
+
+    }
+}
