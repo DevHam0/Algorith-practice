00239e0a0e98f697a148bd7c695d579b7d9166f2
backwizard0 <gkaeogus7@gmail.com>
2023-05-19 20:43:56 +0900

    mmd

--- src/Part1/Q2744.java
+++ src/Part1/Q2744.java
@@ -2,6 +2,10 @@ package Part1;
 
 import java.util.Scanner;
 
+    // 문제: 영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로,
+    // 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+    // 입력: WrongAnswer
+    // 출력: wRONGaNSWER
 public class Q2744 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,8 +18,31 @@ public class Q2744 {
 //        System.out.println((char)65);       // A
 //        System.out.println(x + 25);         // 90
 //        System.out.println((char)(x + 25)); // Z
+//        String str = sc.nextLine();
+//        ASCII Code를 이용한 대소문자 변환
+//        for (int i = 0; i < str.length(); i++) {
+//            // 대문자니?
+//            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+//                // int dist = str.charAt(i) - 'A';
+//                // int lower_ascii = 'A' + dist;
+//                System.out.println((char) ('a' + str.charAt(i) - 'A'));
+//            }
+//            else System.out.println((char)('A' + str.charAt(i) - 'a'));
+//        }
 
-        String input = sc.nextLine();
+
+        //풀이
+        String str = sc.next();
+        char[] ans = str.toCharArray();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if ('A' <= ch && ch <= 'Z'){
+                ans[i] = (char)('a' + ch - 'A');
+            }else{
+                ans[i] = (char)('A' + ch - 'a');
+            }
+            System.out.println(ans);
+        }
 
 
     }
