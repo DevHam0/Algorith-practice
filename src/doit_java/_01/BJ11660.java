00931715841c8cd1f26f2bace95522a94574bca8
admin <gkaeogus7@gmail.com>
2024-01-29 11:31:23 +0900

    240129

--- src/doit_java/_01/BJ11660.java
+++ src/doit_java/_01/BJ11660.java
@@ -26,7 +26,14 @@ public class BJ11660 {
             }
         }
         for (int i = 0; i < M; i++) {
-
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            // 구간 합 배열로 질의에 답변하기
+            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            System.out.println(result);
         }
     }
 }
