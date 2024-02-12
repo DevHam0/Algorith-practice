ed1a8fb1bf40c16d26c9569607470f867b664d74
backwizard0 <gkaeogus7@gmail.com>
2024-02-12 22:38:09 +0900

    240212

--- src/ColumbusStudy/Chap01/N01_BJ16916_240212.java
+++ src/ColumbusStudy/Chap01/N01_BJ16916_240212.java
@@ -1,26 +1,56 @@
 package ColumbusStudy.Chap01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class N01_BJ16916_240212 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String S = sc.nextLine();
-        String P = sc.nextLine();
-        boolean check = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
 
-        for (int i=0; i<S.length(); i++) {
-            if (S.toCharArray()[i] == P.toCharArray()[0]) {
-                for (int j=0; j<P.length();j++) {
-                    if (S.toCharArray()[i+j] != P.toCharArray()[j]) {
-                        check = false;
-                    } else {
-                        check = true;
-                    }
+        System.out.println(KMP(s1, s2));
+    }
+
+    static int KMP(String parent, String pattern) {
+        int[] table = makeTable(pattern);
+        int n1 = parent.length();
+        int n2 = parent.length();
+
+        int idx = 0;
+        for (int i=0; i<n1; i++) {
+            while (idx > 0 && parent.charAt(i) != pattern.charAt(idx)) {
+                idx = table[idx-1];
+            }
+
+            if (parent.charAt(i) == pattern.charAt(idx)) {
+                if (idx == n2-1) {
+                    idx = table[idx];
+                    return 1;
+                }else {
+                    idx += 1;
                 }
             }
         }
-        if (check) System.out.println(1);
-        else System.out.println(0);
+        return 0;
+    }
+
+    static int[] makeTable(String pattern) {
+        int n = pattern.length();
+        int[] table = new int[n];
+        int idx = 0;
+        for (int i=1; i<n; i++) {
+            while(idx>0 && pattern.charAt(i) != pattern.charAt(idx)) {
+                idx = table[idx-1];
+            }
+
+            if (pattern.charAt(i) == pattern.charAt(idx)) {
+                idx += 1;
+                table[i] = idx;
+            }
+        }
+        return table;
     }
 }
---
+++ src/ColumbusStudy/Chap01/N01_BJ9046_240212.java
@@ -0,0 +1,14 @@
+package ColumbusStudy.Chap01;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class N01_BJ9046_240212 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
