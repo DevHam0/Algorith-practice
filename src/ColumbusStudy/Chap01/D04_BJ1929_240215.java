b8a4ed575d9553f0b5088ad30f5afbf0178e7cb0
admin <gkaeogus7@gmail.com>
2024-02-15 11:23:06 +0900

    240215

--- src/ColumbusStudy/Chap01/D04_BJ1929_240215.java
+++ src/ColumbusStudy/Chap01/D04_BJ1929_240215.java
@@ -1,11 +1,10 @@
 package ColumbusStudy.Chap01;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class D04_BJ1929_240215 {
     // M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
     // 첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
     // 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
@@ -15,11 +14,18 @@ public class Main {
         String[] input = word.split(" ");
         int M = Integer.parseInt(input[0]);
         int N = Integer.parseInt(input[1]);
+        String result = "";
         for (int i=M; i<=N; i++) {
             if (getPrimeNumber(i)) {
-                System.out.println(i);
+                if(result.length() != 0) {
+                    result = result + "\n" + i;
+                }else {
+                    result = i + "";
+                }
             }
         }
+
+        System.out.print(result);
     }
 
     static boolean getPrimeNumber(int num) {
